51978cb3dae911112a19c6d46045cd441ade39ea
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-01-24 13:59:10 +0300

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""This is the Square class"""
-
 
 class Square:
-    """Define new instance of the Square class"""
+    """
+    This is the Square class.
+
+    The Square class represents a square object with a private
+    instance attribute 'size'.
+    """
     def __init__(self, size):
-        """Size(int): The size of the square"""
-        self.___size = size
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
