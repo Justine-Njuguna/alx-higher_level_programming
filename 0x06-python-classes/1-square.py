e2475e616221c76d1491d77003490a37564308e5
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-01-24 13:55:00 +0300

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is the Square class"""
+
+
+class Square:
+    """Define new instance of the Square class"""
+    def __init__(self, size):
+        """Size(int): The size of the square"""
+        self.___size = size
