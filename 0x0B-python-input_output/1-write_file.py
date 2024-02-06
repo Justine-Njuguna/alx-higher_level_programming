aacfd7500504773ef7224ef1cb35de0a331270c9
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-02-06 11:53:59 +0300

    Task-1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to write a string to a text file."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string a text-file(utf-8) and returns the number of chars"""
+    
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
