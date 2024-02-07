19709a93ad0b04dcb5edef5cf229866bb3289f9f
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-02-07 06:01:01 +0300

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Class Student w attributes"""
+
+
+class Student:
+    """Defines a student by first_name, last_name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Inits a Student instance with first_name, last_name, and age."""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance."""
+
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+
+        if attrs is None:
+            return self.__dict__
+
+        else:
+            return {
+                    attr: getattr(self, attr)
+                    for attr in attrs
+                    if hasattr(self, attr)
+                    }
