2612cfabc064c25e86f731ba819b2e3f1a1951f0
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-03-12 23:07:29 +0300

    Best Score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- List all records with score >= 10 in the second_table ordered by score (top first)
 SELECT score, name
-FROM hbtn_0c_0.second_table
+FROM 'second_table'
 WHERE score >= 10
 ORDER BY score DESC;
