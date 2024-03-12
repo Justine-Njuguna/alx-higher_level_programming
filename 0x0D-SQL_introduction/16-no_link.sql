1006ec0fce9aad85281a3d66547d886b241f6711
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-03-12 23:14:03 +0300

    No links in SQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- List all records with a name value from the second_table
-SELECT score, name
-FROM hbtn_0c_0.second_table
-WHERE name IS NOT NULL
-ORDER BY score DESC;
+SELECT `score, name`
+FROM `second_table`
+WHERE `name` IS NOT NULL
+ORDER BY `score` DESC;
