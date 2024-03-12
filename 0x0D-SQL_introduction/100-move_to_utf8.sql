0b5c51d1ac7a3f6521b39b93a99c1c4cb38a0c52
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-03-12 23:17:44 +0300

    Advanced 1

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,7 @@
--- Convert database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- converts hbtn_0c_0 database to UTF8.
 
--- Convert table to UTF8
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
 
--- Convert field to UTF8
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table`
+
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
