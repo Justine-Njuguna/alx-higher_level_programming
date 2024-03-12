1164bd0a3ea887f6eb2c9ecec0989fe8336001c2
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-03-12 22:58:10 +0300

    Full-Tables

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,3 @@
 -- Print the full description of the table
 
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM information_schema.columns
-WHERE TABLE_SCHEMA = '$1' AND TABLE_NAME = 'first_table';
-
+SHOW CREATE TABLE `first_tablE`;
