9cf53bf8a56283210ce2d2e95487781d3b59308c
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-03-17 19:26:11 +0300

    Task-6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,13 @@
+-- Task 6: Create database hbtn_0d_usa and table states
+
+-- Create database hbtn_0d_usa if it doesn't already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Create table states if it doesn't already exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
