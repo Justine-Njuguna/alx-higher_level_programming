12a2175560d50585ff5ac781b47d7a8e2de8af30
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-04-14 11:24:44 +0300

    Task-1-wueh

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,54 +1,21 @@
 #!/usr/bin/python3
-""" lists all states beginning with N """
+""" Task-1 lists all states beginning with N """
 
 import sys
 import MySQLdb
 
-
-def connect_to_database(user, password, database):
-    """Connects to the MySQL database.
-    """
-    try:
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=user,
-                passwd=password,
-                db=database
-                )
-        cs = db.cursor()
-        return db, cs
-    except MySQLdb.Error as err:
-        print(f"Error connecting to MySQL database: {err}")
-        sys.exit(1)
-
-
-def fetch_states(cursor):
-    """Fetches states starting with 'N' from the database.
-    """
-    cursor.execute("
-    SELECT * FROM `states`
-    WHERE name LIKE 'N%'
-    ORDER BY id ASC"
-    )
-    return cursor.fetchall()
-
-
-def print_states(states):
-    """Prints each state information on a separate line.
-    """
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            host="localhost",
+            port=3306)
+    cs = db.cursor()
+    cs.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cs.fetchall()
     for state in states:
         print(state)
 
-
-def main():
-    """Main function to connect, fetch, and print states."""
-
-    if __name__ == "__main__":
-        db, cs = connect_to_database(sys.argv[1], sys.argv[2], sys.argv[3])
-        states = fetch_states(cs)
-        print_states(states)
-        cs.close()
-        db.close()
-    if __name__ == "__main__":
-        main()
+    cs.close()
+    db.close()
