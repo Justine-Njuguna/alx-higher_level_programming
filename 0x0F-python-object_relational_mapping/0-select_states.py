d31fcf0ca5643d0ea4dc7ae6709211e0c9b2e36f
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-04-15 06:20:12 +0300

    Solved the password error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,51 +1,13 @@
 #!/usr/bin/python3
-import MySQLdb
+""" Lists all states from the database hbtn_0e_0_usa Task-0 """
+
+
 import sys
+import MySQLdb
 
-def main():
-    # Check if correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Extract command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Connect to MySQL server
-    try:
-        conn = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=mysql_username,
-                passwd=mysql_password,
-                db=database_name
-                )
-        cur = conn.cursor()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    # Execute SQL query
-    try:
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        
-        # Fetch and display results
-        for row in cur.fetchall():
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close cursor and connection
-        if cur:
-            cur.close()
-        if conn:
-            conn.close()
 
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
             passwd=sys.argv[2],
             db=sys.argv[3],
             host="localhost",
-            port=3306)
+            port=3306,
+            charset="utf8mb4"
+            )
     cs = db.cursor()
     cs.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY id ASC")
     states = cs.fetchall()
