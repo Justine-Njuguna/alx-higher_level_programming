6b45fb2b444e96abfb3309cb98fa6150ae883cca
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-07-16 00:44:54 +0300

    Task-Sqauare.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module for the Square class
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    The Sqyare class
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Square class constructor
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        String representation
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
